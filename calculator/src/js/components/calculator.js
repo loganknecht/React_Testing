694affec0c732957b62f500bc78889550ab0196c
Logan Knecht <lknecht@splunk.com>
2016-10-23 22:49:20 -0700

    Things.

---
+++ calculator/src/js/components/calculator.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var calculator = React.CreateClass({
+    render: function() {
+        var className="calculator";
+        return (
+            <div className={className}>
+            Heyo! I'm a calculator!
+            </div>
+        );
+    }
+});
---
+++ calculator/src/js/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Third-Party Libraries
+import React from "react";
+import ReactDOM from "react-dom";
+// Custom Libraries
+import Calculator from "./components/calculator";
+// Styling
+import "../../css/index.css";
+
+ReactDOM.render(
+  <Calculator />,
+  document.getElementById("root")
+);
